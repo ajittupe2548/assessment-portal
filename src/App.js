0ff1f183dc42d193ff52983a9ca7375c327521bb
Ajit Tupe <ajittupe2548@gmail.com>
2023-10-07 17:16:42 +0530

    New assessment card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import styles from './app.scss';
+import NewAssessment from './components/NewAssessment';
 
 const app = () => {
     return (
-        <h1 className={styles.heading}>Assessment portal</h1>
+        <NewAssessment />
     )
 }
 
---
+++ src/components/NewAssessment.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import styles from './scss/NewAssessment.module.scss';
+
+function NewAssessMent() {
+    return (
+        <>
+            <div className={styles.container}>
+                <span className={styles.iconContainer}>
+                    <svg width="28" height="28" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16.8334 10.1905H9.69056V17.3333H7.30961V10.1905H0.166748V7.80951H7.30961V0.666656H9.69056V7.80951H16.8334V10.1905Z" fill="#0073E6" />
+                    </svg>
+
+                </span>
+
+                <p className={styles.title}>
+                    New Assessment
+                </p>
+
+                <p className={styles.description}>
+                    From here you can add questions of multiple types like MCQs, subjective (text or paragraph)!
+                </p>
+
+            </div>
+        </>
+    )
+}
+
+export default NewAssessMent
